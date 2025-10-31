2848b8a3f0fca9af2bc941eaa844f03577ed37bb
Artorion <arthur.adorno4@gmail.com>
2025-10-31 10:53:11 -0300

    .

--- Módulo3 - PHP/CRUD - Alunos/paginas/cadastro.php
+++ Módulo3 - PHP/CRUD - Alunos/paginas/cadastro.php
@@ -43,35 +43,56 @@
             </select>
 
             <input type="submit" value="Cadastrar">
-
         </form>
     </main>
 
     <?php
 
-       if($_SERVER("REQUEST_METHOD") == "POST"){
+       try {
+       if($_SERVER["REQUEST_METHOD"] == "POST") {
         include("../conexao/conexao.php");
 
         $nome = $_POST["nome"];
         $sobrenome = $_POST["sobrenome"];
         $email = $_POST["email"];
         $curso = $_POST["curso"];
+        // criar uma variável com os valores digitados pelo usuário
 
         //Criar
         $hoje = new DateTime();
+        // adquirir a data de hoje
         $id = $hoje->format("Ym").rand(100,999);
+        // formato do id(year/month)
 
         $sql = "INSERT INTO usuario (id,nome,sobrenome,email,curso) VALUES (?,?,?,?,?)";
         $stmt = $conn->prepare($sql);
+        // Criar conexão com o banco de dados
         $stmt->bind_param("issss",$id,$nome,$sobrenome,$email,$curso);
-        $stm->execute();
+        // paramatreos a serem inseridos
+        $stmt->execute();
 
         echo "<div class='mensagem sucesso'>Usuário cadastrado com sucesso</div>";
 
         $stmt->close();
         $conn->close();
+        // fechar o código apos a execucação
+       }
+
 
        }
+
+       catch(mysqli_sql_exception $e){
+        // Duplicate entry
+        if (str_contains($e->getMessage(), "Duplicate entry")) {
+        // str_contains = if 'in'
+            echo "<div class='mensagem erro'>E-mail já está cadastrado</div>";
+        } else {
+            echo "<div class='mensagem erro'>Erro ao cadastrar, Tente novamente mais tarde</div>";
+        }
+        echo $e->getMessage();
+       }
+    //    correção de erros
+
 
 
     ?>
